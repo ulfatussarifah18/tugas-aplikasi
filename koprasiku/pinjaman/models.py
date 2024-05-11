f7fe54663e66b14e725b67d834cc0ac853aee265
ulfatussarifah18 <ulfatussarifah18@gmail.com>
2024-05-11 15:39:02 +0700

    merelasikan 2 models  pinjaman dan simpanan ke models anggota

--- koprasiku/pinjaman/models.py
+++ koprasiku/pinjaman/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from anggota.models import Anggota
 
 class pinjaman(models.Model):
-    nama= models.CharField(max_length=100)
+    nama= models.ForeignKey(Anggota, related_name='pinjaman', on_delete=models.CASCADE)
     nomor_hp = models.CharField(max_length=15, unique=True)
     jumlah_pinjaman= models.CharField(max_length=100)
     jangka_waktu= models.DateField()
     tanggal_pinjaman= models.DateField(auto_now_add=True)
     status_pinjaman= models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.nama
--- koprasiku/simpanan/models.py
+++ koprasiku/simpanan/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from anggota.models import Anggota
+
 
 class simpanan(models.Model):
-    nama= models.CharField(max_length=100)
+    nama= models.ForeignKey(Anggota, related_name='simpanan', on_delete=models.CASCADE)
     nomor_hp = models.CharField(max_length=15, unique=True)
     jenis_simpanan= models.CharField(max_length=100)
     jumlah_simpanan= models.DateField()
